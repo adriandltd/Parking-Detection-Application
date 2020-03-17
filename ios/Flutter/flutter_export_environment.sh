f45196e57b72166bd1b3644d9a776be58ea9feda
Adrian <adriandltd@gmail.com>
2020-03-16 22:27:43 -0500

    iOS update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/adrian/documents/development/flutter"
+export "FLUTTER_ROOT=/Users/adrian/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/adrian/Documents/GitHub/Parking-Detection-Application"
 export "FLUTTER_TARGET=/Users/adrian/Documents/GitHub/Parking-Detection-Application/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/adrian/documents/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/adrian/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
